30de9a3c289eb2c910719b8bad96f415b2b72a6d
faithlawrencex <faith_lawrence@berkeley.edu>
2026-02-04 16:19:17 -0800

    Add files via upload

--- usage_example.py
+++ usage_example.py
@@ -5,15 +5,19 @@ import board
 i2c = board.I2C()
 bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 bme680.sea_level_pressure = 1013.25
+duration = 0
 
-print("hi")
-
-while True:
-	print("Testing")
-	print("Temperature: %0.1f C" % bme680.temperature)
-	print("Gas: %d ohm" % bme680.gas)
+while duration < 5:
+	print("Temperature: %0.1f C" % bme680.temperature) 
+	print("Gas: %d ohm" % bme680.gas) 
 	print("Humidity: %0.1f %%" % bme680.relative_humidity)
-	print("Pressure: %0.3f hPa" % bme680.pressure)
+	print("Pressure: %0.3f hPa" % bme680.pressure"Pressure: %0.3f hPa" % bme680.pressure)
 	print("Altitude = %0.2f meters" % bme680.altitude)
+	print("Temperature: %0.1f C" % bme680.temperature + "Gas: %d ohm" % bme680.gas + "Humidity: %0.1f %%" % bme680.relative_humidity + "Pressure: %0.3f hPa" % bme680.pressure + "Altitude = %0.2f meters" % bme680.altitude) 
+	
+	time_current = time.time()
+	print(f"time: {time_current}")
+	
+	duration += 1
 	time.sleep(2)
 
