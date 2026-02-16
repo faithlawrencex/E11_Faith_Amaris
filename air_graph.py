bed00cedf2ba8db1a9f83c8afaf57d0cd840ccac
amarisch-cell <amarisch@berkeley.edu>
2026-02-16 13:36:44 -0800

    Update air_graph.py
    messed around

--- air_graph.py
+++ air_graph.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('data/air.csv')
-
+plt.figure(figsize=(12, 6))
 plt.hist(data['par05'], bins = 4)
 plt.show()
+
+print(data.head())
