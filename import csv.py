a88f97a26b1fae2545eeaf5ba3e9445df5a100a8
amarisch-cell <amarisch@berkeley.edu>
2026-02-18 14:32:20 -0800

    updated script dk what rly tho

--- import csv.py
+++ import csv.py
@@ -1,16 +1,26 @@
 import csv
+import time
+import numpy as np
+import sys
 
-file = open('test.csv', 'w', newline = None)
+arguments = sys.argv
+print(arguments)
+
+data_path = 'data/' + arguments[1]
+runtime = int(arguments[2])
+
+file = open(data_path, 'w', newline = None)
 
 csvwriter = csv.writer(file, delimiter-',')
 
 meta = ['time','data']
-
 csvwriter.writerow(meta)
 
-for i in range(10):
+for i in range(runtime):
+    time.sleep(2)
     now = time.time()
     value = np.random.random()
     csvwriter.writerow([now, value])
+
 
 file.close()
